6cceedba773fea2e3c213a8244a5a66a987cc33a
Luigi Operoso <40476330+brokenpip3@users.noreply.github.com>
2023-06-25 15:48:36 +0200

    fix(backup): get-latest.sh, search for both .gz and .zstd (#858)

--- backup/pvc/bin/get-latest.sh
+++ backup/pvc/bin/get-latest.sh
@@ -3,8 +3,8 @@
 set -eo pipefail
 
 [[ -z "${BACKUP_DIR}" ]] && echo "Required 'BACKUP_DIR' env not set" && exit 1
-
-latest=$(find ${BACKUP_DIR} -name '*.tar.zstd' -exec basename {} \; | sort -g | tail -n 1)
+# Search for all the tar.* inside the backup dir to support the migration between gzip vs zstd
+latest=$(find ${BACKUP_DIR} -name '*.tar.*' -exec basename {} \; | sort -g | tail -n 1)
 
 if [[ "${latest}" == "" ]]; then
   echo "-1"
